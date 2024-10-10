ac692563bf98b0ee4901c348f3e01cd37fcc2f4e
Jebbs-dev <ojebiyifulness@gmail.com>
2024-10-10 22:12:10 +0100

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,7 @@
-import { useState } from "react";
-import { MyList } from "./components/my-list";
-import { MyButton } from "./components/my-button";
+import PostPage from "./examples/posts/posts-page";
 
-const names = [
-  "Alice",
-  "Bob",
-  "Charlie",
-  "David",
-  "Eve",
-  "Frank",
-  "Grace",
-  "Henry",
-  "Isabella",
-  "Jack",
-  "Kate",
-  "Liam",
-  "Mia",
-  "Noah",
-  "Olivia",
-  "Peter",
-  "Quinn",
-  "Rachel",
-  "Sam",
-  "Sophia",
-  "Tim",
-  "Ursula",
-  "Victor",
-  "Wendy",
-  "Xavier",
-  "Yolanda",
-  "Zoe",
-];
-
-function App() {
-  const [revealedNames, setRevealedNames] = useState([]);
-
-  const revealNextName = () => {
-    if (revealedNames.length < names.length) {
-      setRevealedNames([...revealedNames, names[revealedNames.length]]);
-    }
-  };
-
-  return (
-    <div className="grid grid-cols-2 m-7 border-blue-500 border-2 p-16 text-center">
-      <div>
-        <MyList revealedNames={revealedNames} />
-
-        {revealedNames.length === names.length && (
-          <p>These are all the names of the award winners</p>
-        )}
-      </div>
-      <div>
-        <MyButton revealNextName={revealNextName} revealedNames={revealedNames} names={names} />
-      </div>
-    </div>
-  );
-}
+const App = () => {
+  return <PostPage />;
+};
 
 export default App;
